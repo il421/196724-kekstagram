fde3534a8142cc7dd2b12a285847811ebd17b290
il421 <il421@mail.ru>
2017-01-26 23:29:01 +0300

    Step two

--- js/form.js
+++ js/form.js
@@ -3,6 +3,10 @@
 var uploadOverlay = document.querySelector('.upload-overlay');
 var uploadFile = document.querySelector('.upload-input');
 var uploadFormCancel = document.querySelector('.upload-form-cancel');
+var filterImagePreview = document.querySelector('.filter-image-preview');
+var filterChrome = document.querySelector('#upload-filter-chrome');
+var controls = document.querySelectorAll('.upload-filter-controls input');
+
 
 if (uploadFile.required) {
   uploadOverlay.classList.remove('invisible');
@@ -11,3 +15,9 @@ if (uploadFile.required) {
 uploadFormCancel.addEventListener('click', function () {
   uploadOverlay.classList.add('invisible');
 });
+
+for (var i = 0; i < controls.length; i++) {
+  controls[i].addEventListener('click', function () {
+    filterImagePreview.classList.add('filter-chrome');
+  });
+}
