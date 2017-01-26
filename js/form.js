f33df361bbd4ea6ed897b4c226d3a91709708a3e
Ilya Suglobov <il421@mail.ru>
2017-01-26 16:46:59 +0300

    Step-one

--- js/form.js
+++ js/form.js
@@ -1,1 +1,13 @@
+'use strict';
+
 var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadFile = document.querySelector('.upload-input');
+var uploadFormCancel = document.querySelector('.upload-form-cancel');
+
+if (uploadFile.required) {
+  uploadOverlay.classList.remove('invisible');
+}
+
+uploadFormCancel.addEventListener('click', function () {
+  uploadOverlay.classList.add('invisible');
+});
