bafd8183e97953176ea7b61e4d0db9955fb0b722
il421 <il421@mail.ru>
2017-01-26 21:30:41 +0300

    1

--- js/form.js
+++ js/form.js
@@ -1,1 +1,9 @@
+'use strict';
+
 var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadFile = document.querySelector('#upload-file');
+var uploadSelectImage = document.querySelector('#upload-select-image');
+
+if (uploadFile.required) {
+  uploadOverlay.classList.add('invisible');
+}
