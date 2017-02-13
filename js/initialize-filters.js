94bc50dbc66db6d76ddc08e58d039bc66ef53dc7
il421 <il421@mail.ru>
2017-02-14 01:22:46 +0300

    Task_1_corrections + Part of Task_2

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,6 +1,5 @@
 'use strict';
-var photo = document.querySelector('.filter-image-preview');
 
 window.initializeFilters = function (evt) {
-  photo.className = 'filter-image-preview ' + evt.target['htmlFor'].substring(7);
+  window.photo.className = 'filter-image-preview ' + evt.target['htmlFor'].substring(7);
 };
--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var photo = document.querySelector('.filter-image-preview');
 var controlDec = document.querySelector('.upload-resize-controls-button-dec');
 var controlInc = document.querySelector('.upload-resize-controls-button-inc');
 var controlValue = document.querySelector('.upload-resize-controls-value');
@@ -12,28 +13,25 @@ var min = 25;
 var step = 25;
 
 var increaseScale = controlInc.addEventListener('click', function () {
-  if (window.valueDefault < max) {
-    window.valueDefault = (window.valueDefault + step);
-    if (window.scale < 1) {
-      window.scale = window.scale + 0.25;
-    }
-    createScale();
+  if (valueDefault < max && scale < 1) {
+    valueDefault = valueDefault + step;
+    scale = scale + 0.25;
   }
+  changeScale();
 });
 
 var decreaseScale = controlDec.addEventListener('click', function () {
-  if (window.valueDefault > min) {
-    window.valueDefault = (window.valueDefault - step);
-    if (window.scale > 0.25) {
-      window.scale = window.scale - 0.25;
-    }
-    createScale();
+  if (valueDefault > min && scale > 0.25) {
+    valueDefault = valueDefault - step;
+    scale = scale - 0.25;
   }
+  changeScale();
 });
 
-var createScale = function (param1, param2) {
+// ФУНКЦИЯ ОБРАТНОГО ВЫЗОВА
+var changeScale = function (callback1, callback2) {
   controlValue.value = valueDefault + '%';
-  window.photo.style.transform = 'scale(' + (scale) + ')';
+  photo.style.transform = 'scale(' + (scale) + ')';
 };
 
-createScale(increaseScale, decreaseScale);
+changeScale(increaseScale, decreaseScale);
