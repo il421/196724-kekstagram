0e51174fd554176768d09579e46d0a838bb29fca
il421 <il421@mail.ru>
2017-02-18 19:51:07 +0300

    Task_1--1

--- js/load.js
+++ js/load.js
@@ -1,5 +1,13 @@
 'use strict';
 
-window.load = function () {
+window.load = (function () {
+  var ACADEMY_DATE = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
 
-};
+  return function (onload) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onload);
+
+    xhr.open('GET', ACADEMY_DATE);
+    xhr.send();
+  };
+})();
