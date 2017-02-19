f99f6abdf648abcb7340d7cddabc3e6ac2c27156
il421 <il421@mail.ru>
2017-02-19 22:44:43 +0300

    Task_1--8--Corrections

--- js/load.js
+++ js/load.js
@@ -1,12 +1,9 @@
 'use strict';
-
-window.load = (function () {
+(function () {
   var ACADEMY_DATE = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
-
-  return function (onload) {
+  window.load = function (onload) {
     var xhr = new XMLHttpRequest();
     xhr.addEventListener('load', onload);
-
     xhr.open('GET', ACADEMY_DATE);
     xhr.send();
   };
